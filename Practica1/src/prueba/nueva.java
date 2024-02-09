98c754a5971f3a5ab993aa808087700d51300c9a
Santig13 <117572417+Santig13@users.noreply.github.com>
2024-02-09 15:32:15 +0100

    uguidfguisd

--- Practica1/src/prueba/nueva.java
+++ Practica1/src/prueba/nueva.java
@@ -1,5 +1,25 @@
 package prueba;
+import javax.swing.*;
+import org.math.plot.*;
 
 public class nueva {
-
+	
+	 public static void main(String[] args) {
+	  // define your data
+		 int i=2;
+	  double[] x = { 1,, 3, 4, 5, 6 };
+	  double[] y = { 45, 89, 6, 32, 63, 12 };
+	  // create your PlotPanel (you can use it as a JPanel)
+	  Plot2DPanel plot = new Plot2DPanel();
+	  // define the legend position
+	  plot.addLegend("SOUTH");
+	  // add a line plot to the PlotPanel
+	  plot.addLinePlot("my plot", x, y);
+	  // put the PlotPanel in a JFrame like a JPanel
+	  JFrame frame = new JFrame("a plot panel");
+	  frame.setSize(600, 600);
+	  frame.setContentPane(plot);
+	  frame.setVisible(true);
+	 }
+ 
 }
---
+++ Practica1/src/prueba/principal.java
@@ -0,0 +1,41 @@
+package prueba;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class principal extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					principal frame = new principal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public principal() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+	}
+
+}
