b9d52cbd313b9aa0deabfc360b1f8893f153b020
Dcouto07 <dcouto@ucm.es>
2024-02-09 15:35:14 +0100

    pRUEBA

--- Practica1/src/prueba/nueva.java
+++ Practica1/src/prueba/nueva.java
@@ -7,7 +7,7 @@ public class nueva {
 	 public static void main(String[] args) {
 	  // define your data
 		 int i=2;
-	  double[] x = { 1,, 3, 4, 5, 6 };
+	  double[] x = { 1,2, 3, 4, 5, 6 };
 	  double[] y = { 45, 89, 6, 32, 63, 12 };
 	  // create your PlotPanel (you can use it as a JPanel)
 	  Plot2DPanel plot = new Plot2DPanel();
